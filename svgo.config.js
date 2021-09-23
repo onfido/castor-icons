35e5d66cb8c71b7750a8f71dc001f1dde7872450
Andre Rabello <andre.rabello@onfido.com>
2021-09-23 13:14:23 +0100

    style: prettier on svgo.config

--- svgo.config.js
+++ svgo.config.js
@@ -1,50 +1,19 @@
 module.exports = {
   multipass: true,
   plugins: [
-    {
-      name: 'cleanupAttrs',
-    },
-    {
-      name: 'collapseGroups',
-    },
-    {
-      name: 'convertShapeToPath',
-    },
-    {
-      name: 'convertStyleToAttrs',
-    },
-    {
-      name: 'inlineStyles',
-    },
-    {
-      name: 'minifyStyles',
-    },
-    {
-      name: 'removeAttrs',
-      params: {
-        attrs: '(fill|stroke)',
-      },
-    },
-    {
-      name: 'removeComments',
-    },
-    {
-      name: 'removeDoctype',
-    },
-    {
-      name: 'removeEmptyAttrs',
-    },
-    {
-      name: 'removeEmptyContainers',
-    },
-    {
-      name: 'removeEmptyText',
-    },
-    {
-      name: 'removeMetadata',
-    },
-    {
-      name: 'sortAttrs',
-    },
+    { name: 'cleanupAttrs' },
+    { name: 'collapseGroups' },
+    { name: 'convertShapeToPath' },
+    { name: 'convertStyleToAttrs' },
+    { name: 'inlineStyles' },
+    { name: 'minifyStyles' },
+    { name: 'removeAttrs', params: { attrs: '(fill|stroke)' } },
+    { name: 'removeComments' },
+    { name: 'removeDoctype' },
+    { name: 'removeEmptyAttrs' },
+    { name: 'removeEmptyContainers' },
+    { name: 'removeEmptyText' },
+    { name: 'removeMetadata' },
+    { name: 'sortAttrs' },
   ],
 };
